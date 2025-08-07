b044b05fe7719dc7ba34d08989f95795b84fb971
nishant-2003 <njha32371@gmail.com>
2025-08-07 13:08:03 +0530

    Delete EmbeddingModels directory

--- EmbeddingModels/embedding_groq_query.py
+++
@@ -1,8 +0,0 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-embedding=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-text=[
-    "My name is Nishant Kumar Jha",
-    "I am the only one here"
-]
-vector=embedding.embed_documents(text)
-print(str(vector))

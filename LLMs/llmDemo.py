2b052152d6927b67ab232b452930e99e247222f3
nishant-2003 <njha32371@gmail.com>
2025-08-07 13:08:15 +0530

    Delete LLMs directory

--- LLMs/llmDemo.py
+++
@@ -1,9 +0,0 @@
-from langchain_groq import ChatGroq
-from dotenv import load_dotenv
-import os
-load_dotenv()
-api_key=os.getenv("GROQ_API_KEY")
-
-llm=ChatGroq(model="llama3-70b-8192", api_key=api_key)
-result=llm.invoke("What is the capital of India?")
-print(result)
